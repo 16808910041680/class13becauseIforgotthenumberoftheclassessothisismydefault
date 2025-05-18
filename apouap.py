33412cc7bc53dbb5d78eda297fbdc7c31288aae0
16808910041680 <forforfirefire@gmail.com>
2025-05-18 10:48:17 +1000

    oi

--- apouap.py
+++ apouap.py
@@ -7,10 +7,12 @@ def main():
     amount = int(input("Enter the number of people: "))
     tip = int(input("Enter the tip percentage: "))
     total = total_calc(bill, amount, tip)
-    print(f"Each person should pay: ${total}")
+    print(f"Each person should pay (including tip): ${total}")
     print ("The total bill is: $", bill)
     print ("The tip is: $", bill * (tip / 100))
     print ("The total amount is: $", bill + (bill * (tip / 100)))
+    print ("Each person will be tipping: $", bill * (tip / 100) / amount)
+    print ("Each person will be paying (excluding tip): $", bill / amount)
 
 main()
 
