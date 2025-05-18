43ef42a8c758fed01eb33f196b9ea51d58dade14
16808910041680 <forforfirefire@gmail.com>
2025-05-18 10:46:33 +1000

    ll

---
+++ apouap.py
@@ -0,0 +1,16 @@
+def total_calc (bill, amount, tip):
+    total = bill + (bill * (tip / 100))
+    return round(total / amount, 2)
+def main():
+    print("Welcome to the tip calculator!")
+    bill = float(input("Enter the total bill amount: "))
+    amount = int(input("Enter the number of people: "))
+    tip = int(input("Enter the tip percentage: "))
+    total = total_calc(bill, amount, tip)
+    print(f"Each person should pay: ${total}")
+    print ("The total bill is: $", bill)
+    print ("The tip is: $", bill * (tip / 100))
+    print ("The total amount is: $", bill + (bill * (tip / 100)))
+
+main()
+
